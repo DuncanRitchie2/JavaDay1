dc9900083026a07d87f57bafc2c7e9bb41e6046c
DuncanRitchie <duncanritchie@btinternet.com>
2019-08-13 14:46:29 +0100

    Multiplicative persistence, palindrome check, prime check

--- Edabit.java
+++ Edabit.java
@@ -7,17 +7,41 @@ import java.util.Arrays;
 
 public class Edabit {
     // Input two Strings, and the return is whether they are anagrams (boolean).
-    public boolean areAnagrams(String one, String two) {
+    public static boolean areAnagramsUsingStringMethod(String one, String two) {
+        one = one.toLowerCase();
+        two = two.toLowerCase();
         String[] array1 = one.split("");
         Arrays.sort(array1);
         String[] array2 = two.split("");
         Arrays.sort(array2);
-        return array1.toString().equals(array2.toString());
+        String sorted1 = StringMethods.toSeparatedString(array1,"");
+        String sorted2 = StringMethods.toSeparatedString(array2,"");
+        return sorted1.equals(sorted2);
+    }
+
+    public static boolean areAnagrams(String s1, String s2) {
+        if (s1.length() == s2.length()) {
+            s1 = s1.toLowerCase();
+            s2 = s2.toLowerCase();
+            String[] array1 = s1.split("");
+            Arrays.sort(array1);
+            String[] array2 = s2.split("");
+            Arrays.sort(array2);
+            String ss1 = "";
+            String ss2 = "";
+            for (int i = 0; i < s1.length(); i++) {
+                ss1 += array1[i];
+                ss2 += array2[i];
+            }
+            return ss1.equals(ss2);
+        } else {
+            return false;
+        }
     }
 
     // Input a String, the return is In aLtErNaTiNg cApS.
-    public String alternatingCaps(String s) {
-        String[] array = s.split("");
+    public static String alternatingCaps(String string) {
+        String[] array = string.split("");
         for (int i = 0; i < array.length; i++) {
             if (i%2 == 0) {
                 array[i] = array[i].toUpperCase();
@@ -26,6 +50,71 @@ public class Edabit {
                 array[i] = array[i].toLowerCase();
             }
         }
-        return array.toString();
+        return StringMethods.toSeparatedString(array,"");
+    }
+
+    public static int numDigits(int num) {
+        int numberOfDigits = 1;
+        while (num/10>1){
+            num = num/10;
+            numberOfDigits ++;
+        }
+        return numberOfDigits;
+    }
+
+    public static int multiplicativePersistence(int num) {
+        int persistence = 0;
+        int product = num;
+
+        while (product >= 10) {
+          String string = ""+product;
+          String[] array = string.split("");
+          int tempProduct = 1;
+          for (String character : array) {
+              int digit = 0;
+              switch (character) {
+                  case "9": digit ++;
+                  case "8": digit ++;
+                  case "7": digit ++;
+                  case "6": digit ++;
+                  case "5": digit ++;
+                  case "4": digit ++;
+                  case "3": digit ++;
+                  case "2": digit ++;
+                  case "1": digit ++;
+                  default: digit += 0;
+              }
+              tempProduct *= digit;
+          }
+          product = tempProduct;
+          persistence ++;
+        }
+
+        return persistence;
+    }
+
+    public static boolean isPalindrome(String str) {
+        boolean bool = true;
+        while (str.length() >1 && bool) {
+            String[] array = str.split("");
+            if (array[0].equals(array[array.length-1])) {
+                str = str.substring(1,array.length-2);
+            }
+            else {
+                bool = false;
+            }
+        }
+        return bool;
+    }
+
+    public static boolean isPrime(int num) {
+        boolean bool = num>1;
+        for (int i = 2; i < num/2+1; i++) {
+            if (num%i==0) {
+                bool = false;
+                break;
+            }
+        }
+        return bool;
     }
 }
--- Main.java
+++ Main.java
@@ -4,13 +4,45 @@ package com.duncanritchie;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Here's a random integer from 1 to 10: "+(int)Math.ceil(Math.random()*10));
-        Bird fulmar = new Bird("fulmarr","Fulmarus glacialis",true,"It roosts on cliffs.","squawk");
-        fulmar.setName("fulmar");
-        fulmar.makeNoise();
-        fulmar.setSpecies("Fulmarus");
-        fulmar.setSpecies("Fulmarus glacialis");
-        fulmar.setHaveISeenIt(true);
-        fulmar.printDesc();
+//        System.out.println("Here's a random integer from 1 to 10: "+(int)Math.ceil(Math.random()*10));
+//
+//        System.out.println("Are Duncanus Richardus and Durans Ac Hinc Durus anagrams?   "+Edabit.areAnagrams("Duncanus Richardus","Durans Ac Hinc Durus"));
+//        System.out.println("Are DuncanusRichardus and DuransAcHincDurus anagrams?       "+Edabit.areAnagrams("DuncanusRichardus","DuransAcHincDurus"));
+//
+//        System.out.println("My name in alternating caps:                                "+Edabit.alternatingCaps("Duncan Ritchie"));
+//        System.out.println("Carmen Buranum in alternating caps:                         "+Edabit.alternatingCaps("o fortuna, velut luna tu es variabilis. semper crescens aut decrescens vita detestabilis."));
+//
+//        System.out.println("Persistence of 999                                          "+Edabit.multiplicativePersistence(999));
+//        System.out.println("Persistence of 25                                           "+Edabit.multiplicativePersistence(25));
+//        System.out.println("Persistence of 4                                            "+Edabit.multiplicativePersistence(4));
+//        System.out.println("Persistence of 39                                           "+Edabit.multiplicativePersistence(39));
+
+//        System.out.println("Is naan a palindrome?                                       "+Edabit.isPalindrome("naan"));
+//        System.out.println("Is fulmar a palindrome?                                     "+Edabit.isPalindrome("fulmar"));
+//        System.out.println("Is racecar a palindrome?                                    "+Edabit.isPalindrome("racecar"));
+//        System.out.println("Is the empty string a palindrome?                           "+Edabit.isPalindrome(""));
+//        System.out.println("Is me a palindrome?                                         "+Edabit.isPalindrome("me"));
+//        System.out.println("Is hat a palindrome?                                        "+Edabit.isPalindrome("hat"));
+//        System.out.println("Is A a palindrome?                                          "+Edabit.isPalindrome("A"));
+
+//        System.out.println("How many digits does 999 have?                              "+Edabit.numDigits(999));
+//        System.out.println("How many digits does 25 have?                               "+Edabit.numDigits(25));
+//        System.out.println("How many digits does 4 have?                                "+Edabit.numDigits(4));
+//        System.out.println("How many digits does 39 have?                               "+Edabit.numDigits(39));
+        System.out.println("Prime numbers up to 1000:");
+        for (int i = 0; i < 1000; i++) {
+            if (Edabit.isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+
+
+//        Bird fulmar = new Bird("fulmarr","Fulmarus glacialis",true,"It roosts on cliffs.","squawk");
+//        fulmar.setName("fulmar");
+//        fulmar.makeNoise();
+//        fulmar.setSpecies("Fulmarus");
+//        fulmar.setSpecies("Fulmarus glacialis");
+//        fulmar.setHaveISeenIt(true);
+//        fulmar.printDesc();
     }
 }
